fa9ec0dfdc53df6fde7017f4b2f47546076c183b
Andrew Archer <andrew.m.archer@gmail.com>
2014-12-07 14:32:01 -0800

    revised styles

--- packgenerator/app/assets/javascripts/application.js
+++ packgenerator/app/assets/javascripts/application.js
@@ -13,5 +13,5 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require_tree ../../../vendor/assets/javascripts/.
+//= require ../../../vendor/assets/javascripts/jquery.gridster.with-extras
 //= require_tree .
